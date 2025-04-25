da1a3d273c0b4567564d220ac7aa3a03ebb40a65
isaka-12 <isakamtweve69@gmail.com>
2025-04-25 15:20:03 +0300

    update

--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -62,10 +62,10 @@ const Login = () => {
           >
             <div className="space-y-4">
               <div>
-                <Label htmlFor="email">Email address</Label>
+                <Label htmlFor="email">Username</Label>
                 <Input
                   id="email"
-                  type="email"
+                  type="text"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   required
